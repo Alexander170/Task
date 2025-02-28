99392ba6fbc0129594f33badf2eec3b65c25e6d0
Alexander Ershov <lancaster1@ro.ru>
2025-02-28 22:08:45 +0300

    add first logic on modal dashboard

--- angular/src/app/pages/layout/layout.component.ts
+++ angular/src/app/pages/layout/layout.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { AddEditComponent } from './add-edit/add-edit.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, MatDialogModule, MatFormFieldModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+
+  constructor(private _dialog: MatDialog) {
+
+  }
+
+  openAddEditEmpForm() {
+    this._dialog.open(AddEditComponent)
+  }
 
 }
