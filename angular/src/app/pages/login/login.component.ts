84a819dbe7f7b3a9fcd6c0482f6333e46b88868a
Alexander Ershov <lancaster1@ro.ru>
2025-02-28 13:52:01 +0300

    add registration/autorization

--- angular/src/app/pages/login/login.component.ts
+++ angular/src/app/pages/login/login.component.ts
@@ -1,16 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
-  userObj: any = {
-    userName: '',
+  isLoginView: boolean = true;
+
+  userRegisterObj: any = {
+    login: '',
     password: '',
   }
-}
+
+  userLogin: any = {
+    login: '',
+    password: '',
+  }
+
+  router = inject(Router);
+
+  onRegister() {
+    const isLocalData = localStorage.getItem("angular18Local");
+    if(isLocalData != null) {
+      const localArray = JSON.parse(isLocalData)
+      localArray.push(this.userRegisterObj);
+      localStorage.setItem("angular18Local", JSON.stringify(localArray))
+    } else {
+      const localArray = [];
+      localArray.push(this.userRegisterObj);
+      localStorage.setItem("angular18Local", JSON.stringify(localArray))
+    }
+    // alert('Reg success')
+  }
+
+  onLogin() {
+    const isLocalData = localStorage.getItem("angular18Local");
+    if(isLocalData != null) {
+      const users = JSON.parse(isLocalData)
+      const isUserFound = users.find((m:any) => m.login == this.userLogin.login && m.password == this.userLogin.password);
+      if(isUserFound != undefined) {
+        this.router.navigateByUrl('dashboard')
+      } else {
+        alert('Error data');
+      }
+      } else {
+        alert('No user');
+      }
+    }
+  }
