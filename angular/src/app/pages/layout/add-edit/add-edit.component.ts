f9f3f4e98a67a3be0258e099463b0d48724df2b9
Alexander Ershov <lancaster1@ro.ru>
2025-03-01 11:28:41 +0300

    add pop-up contentt

--- angular/src/app/pages/layout/add-edit/add-edit.component.ts
+++ angular/src/app/pages/layout/add-edit/add-edit.component.ts
@@ -3,13 +3,26 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+
+interface Education {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-add-edit',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatDialogModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatDialogModule, MatDatepickerModule, MatNativeDateModule, MatRadioModule, MatSelectModule],
   templateUrl: './add-edit.component.html',
   styleUrl: './add-edit.component.scss'
 })
 export class AddEditComponent {
-
+  educations: Education[] = [
+    {value: 'Среднее школьное', viewValue: 'Среднее школьное'},
+    {value: 'Среднее специальное', viewValue: 'Среднее специальное'},
+    {value: 'Высшее', viewValue: 'Высшее'},
+  ]
 }
